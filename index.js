a9a335407e74272a2678ceb30bd4f2f11ee304df
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-17 10:46:29 -0400

    commit tree 390bb5dbc62a2598f0f06e2764df2c724490e97b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+/*  console.log("You've been razzled!");
+}
+razzle();
+//=> "You've been razzled!");
+
+razzle(); //=> "You've been razzled!"
+    function razzle() {
+        console.log(You've been razzled!); 
+    }
+*/
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+function mondayWork(action = 'go to the office'){
+    return `This Monday, I will ${action}.`
+}
+mondayWork()
+
+
+
+function wrapAdjective(star = "*"){
+    return function
+    innerFunction(adjective = "special"){
+        return `You are ${star}${adjective}${star}!`;
+    };
+};
+wrappedAdjective();
